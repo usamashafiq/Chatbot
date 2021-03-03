19c5c5dac88b76d445f84fab71f666963b8d2b69
usamashafiq <46091325+usamashafiq@users.noreply.github.com>
2021-03-03 18:00:28 +0500

    Create code.py

---
+++ code.py
@@ -0,0 +1,56 @@
+import spacy
+from chatterbot import ChatBot
+from chatterbot.trainers import ListTrainer
+
+spacy.load('en')
+
+Bookshop = ChatBot('bookshopBot',
+                   read_only=False,
+                   logic_adapters=["chatterbot.logic.BestMatch"],
+                   storage_adapter="chatterbot.storage.SQLStorageAdapter")
+
+greet_conversation = [
+    "Hello",
+    "Hi there!",
+    "How are you?",
+    "I'm great well.",
+    "what is your name?",
+    "my name is book shop robot.",
+    "That is good to hear",
+    "Thank you.",
+    "You're welcome",
+    "Yes it is."
+
+]
+
+open_timings_conversation = [
+    "What time does the Bookshop open?",
+    "The Bookshop opens at 9AM.",
+]
+
+close_timings_conversation = [
+    "What time does the Bookshop close?",
+    "The Bookshop closes at 8PM.",
+]
+spec_conversation = [
+    "How much type of books you have?",
+    "Two thousands type of books I have .",
+    "who is book is good?",
+    "Holly Quran pak is a good book I have. "
+
+]
+# Initializing Trainer Object
+trainer = ListTrainer(Bookshop)
+
+# Training BankBot
+trainer.train(greet_conversation)
+trainer.train(open_timings_conversation)
+trainer.train(close_timings_conversation)
+trainer.train(spec_conversation)
+
+while True:
+    user_input = input('You')
+    if user_input == 'quit':
+        break
+    response = Bookshop.get_response(user_input)
+    print(response)
